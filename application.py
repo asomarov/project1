b03615ec2170461ce8f7acad93ce1800b20c7d04
Askar Omarov <asomarov@gmail.com>
2020-04-03 21:15:35 +0600

    Added error.html, search_results.html, routes in application.html for these web-pages. Search_results.html is not operational due to an error in handling the name of the column in the table books

--- application.py
+++ application.py
@@ -4,6 +4,7 @@ from flask import Flask, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -20,7 +21,29 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-
+headers = db.execute("SELECT * FROM books LIMIT 1").fetchone()
+headerN = [1,2,3,4]
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    #headers = db.execute("SELECT * FROM books LIMIT 1").fetchone()
+    return render_template("index.html", headers = headers, headerN = headerN)
+
+@app.route("/search_results", methods=["POST"])
+def search_results():
+    """Search results"""
+
+    item = request.form.get("search_item")
+
+    try:
+        i = int(request.form.get("i"))
+    except ValueError:
+        return render_template("error.html", message="Nothing found, please try to rephrase your query")
+    res = db.execute("SELECT * FROM books WHERE :header LIKE CONCAT('%',:item,'%')", {"header": header, "item": item}).fetchall()
+
+    if res is None:
+        return render_template("error.html", message="Nothing found, please try to rephrase your query")
+    else:
+        return render_template("search_results.html", res=res)
+@app.route("/more")
+def more():
+    return render_template("more.html")
